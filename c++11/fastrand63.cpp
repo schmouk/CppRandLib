fdbaf9d3430e6529371895678cee3440982061ca
Philippe Schmouker <ph.schmouker@yahoo.fr>
2022-08-25 22:15:49 +0200

    #31 - Implement class `FastRand63` (c++11)
    Completed; Tested.

--- c++11/fastrand63.cpp
+++ c++11/fastrand63.cpp
@@ -22,19 +22,19 @@ SOFTWARE.
 //===========================================================================
 #include <chrono>
 
-#include "fastrand32.h"
+#include "fastrand63.h"
 
 
 //===========================================================================
 /** Sets the internal state of this PRNG from current time (empty signature). */
-void FastRand32::setstate() noexcept
+void FastRand63::setstate() noexcept
 {
     const uint64_t ticks = std::chrono::duration_cast<std::chrono::nanoseconds>(
         std::chrono::high_resolution_clock::now().time_since_epoch()).count();
 
-    const uint32_t t32 = uint32_t(ticks & 0xffff'ffff);
-    MyBaseClass::setstate(((t32 & 0xff00'0000) >> 24) +
-                          ((t32 & 0x00ff'0000) >> 8) +
-                          ((t32 & 0x0000'ff00) << 8) +
-                          ((t32 & 0x0000'00ff) << 24));
+    MyBaseClass::setstate(((ticks & 0x0000'0000'7fff'ffff) << 32) +
+                          ((ticks & 0xff00'0000'0000'0000) >> 56) +
+                          ((ticks & 0x00ff'0000'0000'0000) >> 40) +
+                          ((ticks & 0x0000'ff00'0000'0000) >> 24) +
+                          ((ticks & 0x0000'00ff'0000'0000) >> 8));
 }
--- c++11/fastrand63.h
+++ c++11/fastrand63.h
@@ -28,100 +28,100 @@ SOFTWARE.
 
 //===========================================================================
 /** @brief Fast Linear Congruential Generator - 32 bits.
-* 
-*   Pseudo-random numbers generator - Linear Congruential Generator dedicated  
-*   to  32-bits  calculations with very short period (about 4.3e+09) but very 
+*
+*   Pseudo-random numbers generator - Linear Congruential Generator dedicated
+*   to  32-bits  calculations with very short period (about 4.3e+09) but very
 *   short time computation.
 *   This module is part of library CppRandLib.
-*   
+*
 *   LCG models evaluate pseudo-random numbers suites x(i) as a simple mathem-
-*   atical function of 
-*   
-*       x(i) = (a * x(i-1) + c) mod m 
-*    
-*   Results  are  nevertheless  considered  to  be  poor  as  stated  in  the 
-*   evaluation done by Pierre L'Ecuyer and Richard Simard (Universite de 
-*   Montreal) in 'TestU01: A C Library for Empirical Testing of Random Number 
-*   Generators  -  ACM  Transactions  on Mathematical Software,  vol.33  n.4,  
-*   pp.22-40,  August 2007'.  It is not recommended to use such pseudo-random 
+*   atical function of
+*
+*       x(i) = (a * x(i-1) + c) mod m
+*
+*   Results  are  nevertheless  considered  to  be  poor  as  stated  in  the
+*   evaluation done by Pierre L'Ecuyer and Richard Simard (Universite de
+*   Montreal) in 'TestU01: A C Library for Empirical Testing of Random Number
+*   Generators  -  ACM  Transactions  on Mathematical Software,  vol.33  n.4,
+*   pp.22-40,  August 2007'.  It is not recommended to use such pseudo-random
 *   numbers generators for serious simulation applications.
-*  
-*   The implementation of this LCG 32-bits model is based  on  (a=69069, c=1) 
-*   since  these  two  values  have  evaluated to be the 'best' ones for LCGs 
+*
+*   The implementation of this LCG 32-bits model is based  on  (a=69069, c=1)
+*   since  these  two  values  have  evaluated to be the 'best' ones for LCGs
 *   within TestU01 while m = 2^32.
-*   See FastRand63 for a 2^63 (i.e. about 9.2e+18) period  LC-Generator  with  
-*   low  computation  time  also,  longer  period and quite better randomness 
-*   characteristics than for FastRand32.
-*     
+*   See FastRand63 for a 2^63 (i.e. about 9.2e+18) period  LC-Generator  with
+*   low  computation  time  also,  longer  period and quite better randomness
+*   characteristics than for FastRand63.
+*
 *   Furthermore this class is callable:
-*     FastRand32 rand();
+*     FastRand63 rand();
 *     std::cout << rand() << std::endl;    // prints a uniform pseudo-random value within [0.0, 1.0)
 *     std::cout << rand(a) << std::endl;   // prints a uniform pseudo-random value within [0.0, a)
 *     std::cout << rand(a,b) << std::endl; // prints a uniform pseudo-random value within [a  , b)
-*   
+*
 *   Notice that for simulating the roll of a dice you should program:
-*     FastRand32 diceRoll();
+*     FastRand63 diceRoll();
 *     std::cout << int(diceRoll(1, 7)) << std::endl;  // prints a uniform roll within range {1, ..., 6}
 *
 *     Reminder:
-*   We give you here below a copy of the table of tests for the LCGs that have 
+*   We give you here below a copy of the table of tests for the LCGs that have
 *   been implemented in CppRandLib, as provided in paper "TestU01, ..."  - see
 *   file README.md.
 * +------------------------------------------------------------------------------------------------------------------------------------------------------------------+
 * | PyRabndLib class | TU01 generator name                | Memory Usage    | Period  | time-32bits | time-64 bits | SmallCrush fails | Crush fails | BigCrush fails |
 * | ---------------- | ---------------------------------- | --------------- | ------- | ----------- | ------------ | ---------------- | ----------- | -------------- |
-* | FastRand32       | LCG(2^32, 69069, 1)                |     1 x 4-bytes | 2^32    |    3.20     |     0.67     |         11       |     106     |   *too many*   |
+* | FastRand63       | LCG(2^32, 69069, 1)                |     1 x 4-bytes | 2^32    |    3.20     |     0.67     |         11       |     106     |   *too many*   |
 * | FastRand63       | LCG(2^63, 9219741426499971445, 1)  |     2 x 4-bytes | 2^63    |    4.20     |     0.75     |          0       |       5     |       7        |
 * +------------------------------------------------------------------------------------------------------------------------------------------------------------------+
 *
 *   * _small crush_ is a small set of simple tests that quickly tests some  of
 *   the expected characteristics for a pretty good PRG;
 *
-*   * _crush_ is a bigger set of tests that test more deeply  expected  random 
+*   * _crush_ is a bigger set of tests that test more deeply  expected  random
 *   characteristics
-*   
-*   * _big crush_ is the ultimate set of difficult tests  that  any  GOOD  PRG 
+*
+*   * _big crush_ is the ultimate set of difficult tests  that  any  GOOD  PRG
 *   should definitively pass.
 */
-class FastRand32 : public BaseRandom<uint32_t>
+class FastRand63 : public BaseRandom<uint64_t>
 {
 public:
     //---   Wrappers   ------------------------------------------------------
-    using value_type  = uint32_t;
-    using MyBaseClass = BaseRandom<uint32_t>;
+    using value_type = uint64_t;
+    using MyBaseClass = BaseRandom<uint64_t>;
 
 
     //---   Constructors / Destructor   -------------------------------------
     /** @brief Default Empty constructor. */
-    inline FastRand32() noexcept
+    inline FastRand63() noexcept
         : MyBaseClass()
     {
         setstate();
     }
 
     /** @brief Valued construtor. */
-    inline FastRand32(const uint32_t seed) noexcept
+    inline FastRand63(const uint64_t seed) noexcept
         : MyBaseClass(seed)
     {}
 
     /** @brief Default Copy constructor. */
-    FastRand32(const FastRand32&) noexcept = default;
+    FastRand63(const FastRand63&) noexcept = default;
 
     /** @brief Default Move constructor. */
-    FastRand32(FastRand32&&) noexcept = default;
+    FastRand63(FastRand63&&) noexcept = default;
 
     /** @brief Default Destructor. */
-    virtual ~FastRand32() noexcept = default;
+    virtual ~FastRand63() noexcept = default;
 
     //---   Assignments   ---------------------------------------------------
     /** @brief Default Copy assignment. */
-    FastRand32& operator= (const FastRand32&) noexcept = default;
+    FastRand63& operator= (const FastRand63&) noexcept = default;
 
     /** @brief Default Move assignment. */
-    FastRand32& operator= (FastRand32&&) noexcept = default;
+    FastRand63& operator= (FastRand63&&) noexcept = default;
 
     /** @brief Seed assignment. */
-    inline FastRand32& operator= (const uint32_t seed) noexcept
+    inline FastRand63& operator= (const uint64_t seed) noexcept
     {
         MyBaseClass::setstate(seed);
         return *this;
@@ -134,8 +134,8 @@ public:
     */
     virtual inline const double random() noexcept override
     {
-        _state.seed = 69'069 * _state.seed + 1;  // the modulo 32 bits is automated here
-        return _state.seed / 4'294'967'296.0;
+        _state.seed = (9'219'741'426'499'971'445ULL * _state.seed + 1) & 0x7fff'ffff'ffff'ffffULL;
+        return double(_state.seed / (long double)9'223'372'036'854'775'808.0);
     }
 
     //---   Operations   ----------------------------------------------------
