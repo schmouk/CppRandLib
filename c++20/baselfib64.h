9987ee9edfe3263d268a6abb7dd20f3849dbdc2f
Philippe Schmouker <ph.schmouker@yahoo.fr>
2025-03-15 18:37:16 +0100

    #89-refactor LFib implementations
    c++20 refactored LFibBase. Still to be validated.
    Module `listseedstate.ixx` now refactored also to `listseedstate.h`

--- c++20/baselfib64.h
+++ c++20/baselfib64.h
@@ -1,3 +1,4 @@
+#pragma once
 /*
 MIT License
 
@@ -24,17 +25,10 @@ SOFTWARE.
 
 
 //===========================================================================
-module;
-
-#include <chrono>
+#include <algorithm>
 
 #include "baserandom.h"
-
-
-export module baselfib64;
-
-import fastrand63;
-import listseedstate;
+#include "listseedstate.h"
 
 
 //===========================================================================
@@ -42,33 +36,33 @@ import listseedstate;
 *
 *   Definition of the base class for all LFib pseudo-random generators based
 *   on 64-bits generated numbers.
-*
+* 
 *   This module is part of library CppRandLib.
-*
+*   
 *   Lagged Fibonacci generators LFib( m, r, k, op) use the recurrence
-*
+*   
 *       x(i) = (x(i-r) op (x(i-k)) mod m
-*
+*   
 *   where op is an operation that can be:
 *       + (addition),
 *       - (substraction),
 *       * (multiplication),
 *       ^ (bitwise exclusive-or).
-*
+*   
 *   With the + or - operation, such generators are in fact MRGs. They offer very large
 *   periods  with  the  best  known  results in the evaluation of their randomness, as
 *   stated in the evaluation done by Pierre L'Ecuyer and Richard Simard (Universite de
-*   Montreal)  in  "TestU01:  A  C  Library  for Empirical Testing  of  Random  Number
-*   Generators - ACM Transactions  on  Mathematical  Software,  vol.33 n.4,  pp.22-40,
-*   August 2007".  It  is  recommended  to  use  such pseudo-random numbers generators
+*   Montreal)  in  "TestU01:  A  C  Library  for Empirical Testing  of  Random  Number  
+*   Generators - ACM Transactions  on  Mathematical  Software,  vol.33 n.4,  pp.22-40, 
+*   August 2007".  It  is  recommended  to  use  such pseudo-random numbers generators 
 *   rather than LCG ones for serious simulation applications.
-*
-*   See LFib78,  LFib116,  LFib668 and LFib1340 for long period LFib generators (resp.
-*   2^78,  2^116,  2^668 and 2^1340 periods, i.e. resp. 3.0e+23, 8.3e+34, 1.2e+201 and
-*   2.4e+403 periods) while same computation time and far  higher  precision  (64-bits
-*   calculations) than MRGs,  but more memory consumption (resp. 17,  55, 607 and 1279
+*      
+*   See LFib78,  LFib116,  LFib668 and LFib1340 for long period LFib generators (resp. 
+*   2^78,  2^116,  2^668 and 2^1340 periods, i.e. resp. 3.0e+23, 8.3e+34, 1.2e+201 and 
+*   2.4e+403 periods) while same computation time and far  higher  precision  (64-bits  
+*   calculations) than MRGs,  but more memory consumption (resp. 17,  55, 607 and 1279 
 *   integers).
-*
+*   
 *   Please notice that this class and all its  inheriting  sub-classes  are  callable.
 *   Example:
 * @code
@@ -84,9 +78,9 @@ import listseedstate;
 *     std::cout << int(diceRoll(1, 7))    << std::endl; // prints a uniform roll within range {1, ..., 6}
 *     std::cout << diceRoll.randint(1, 6) << std::endl; // prints also a uniform roll within range {1, ..., 6}
 * @endcode
-*
+* 
 *   Reminder:
-*   We give you here below a copy of the table of tests for the LCGs that have
+*   We give you here below a copy of the table of tests for the LCGs that have 
 *   been implemented in PyRandLib, as provided in paper "TestU01, ..."  -  see
 *   file README.md.
 * +--------------------------------------------------------------------------------------------------------------------------------------------------------+
@@ -101,22 +95,22 @@ import listseedstate;
 *   * _small crush_ is a small set of simple tests that quickly tests some  of
 *   the expected characteristics for a pretty good PRG;
 *
-*   * _crush_ is a bigger set of tests that test more deeply  expected  random
+*   * _crush_ is a bigger set of tests that test more deeply  expected  random 
 *   characteristics;
 *
-*   * _big crush_ is the ultimate set of difficult tests  that  any  GOOD  PRG
+*   * _big crush_ is the ultimate set of difficult tests  that  any  GOOD  PRG 
 *   should definitively pass.
 */
-export template<const size_t SIZE, const size_t K>
-class BaseLFib64 : public BaseRandom<ListSeedState<uint64_t, SIZE>>
+template<const size_t SIZE, const size_t K>
+class BaseLFib64 : public BaseRandom<ListSeedState<std::uint64_t, SIZE>, std::uint64_t, 64>
 {
 public:
     //---   Wrappers   ------------------------------------------------------
-    using value_type = uint64_t;
-    using StateType = ListSeedState<uint64_t, SIZE>;
-    using MyBaseClass = BaseRandom<StateType>;
-
-    static constexpr size_t SEED_SIZE = SIZE;
+    using MyBaseClass = BaseRandom<ListSeedState<std::uint64_t, SIZE>, std::uint64_t, 64>;
+    using output_type = MyBaseClass::output_type;
+    using state_type  = MyBaseClass::state_type;
+    
+    static const size_t SEED_SIZE{ SIZE };
 
 
     //---   Constructors / Destructor   -------------------------------------
@@ -128,7 +122,7 @@ public:
     }
 
     /** @brief Valued construtor (integer). */
-    inline BaseLFib64(const uint64_t seed) noexcept
+    inline BaseLFib64(const std::uint64_t seed) noexcept
         : MyBaseClass()
     {
         setstate(seed);
@@ -142,45 +136,47 @@ public:
     }
 
     /** @brief Valued constructor (full state). */
-    inline BaseLFib64(const StateType& seed) noexcept
+    inline BaseLFib64(const state_type& seed) noexcept
         : MyBaseClass()
     {
         setstate(seed);
     }
 
-    /** @brief Default Copy constructor. */
-    BaseLFib64(const BaseLFib64&) noexcept = default;
-
-    /** @brief Default Move constructor. */
-    BaseLFib64(BaseLFib64&&) noexcept = default;
-
-    /** @brief Default Destructor. */
-    virtual ~BaseLFib64() noexcept = default;
+    BaseLFib64(const BaseLFib64&) noexcept = default;   //!< default copy constructor.
+    BaseLFib64(BaseLFib64&&) noexcept = default;        //!< default move constructor.
+    virtual ~BaseLFib64() noexcept = default;           //!< default destructor.
 
 
     //---   Operations   ----------------------------------------------------
-    /** @brief Sets the internal state of this PRNG from current time (empty signature). */
-    virtual void setstate() noexcept override;
+    /** @brief Sets the internal state of this PRNG from shuffled current time (empty signature). */
+    inline virtual void setstate() noexcept override
+    {
+        setstate(utils::set_random_seed64());
+    }
 
     /** @brief Sets the internal state of this PRNG with an integer seed. */
-    void setstate(const uint64_t seed) noexcept;
+    inline void setstate(const std::uint64_t seed) noexcept
+    {
+        utils::SplitMix64 splitmix_64(seed);
+        std::ranges::generate( MyBaseClass::_state.seed.list, [&]() { return splitmix_64(); } );
+    }
 
     /** @brief Sets the internal state of this PRNG with a double seed. */
     inline void setstate(const double seed) noexcept
     {
         const double s = (seed <= 0.0) ? 0.0 : (seed >= 1.0) ? 1.0 : seed;
-        setstate(uint64_t(s * double(0xffff'ffff'ffff'ffffUL)));
+        setstate(std::uint64_t(s * double(_MODULO)));
     }
 
     /** @brief Restores the internal state of this PRNG from seed. */
-    inline void setstate(const StateType& seed) noexcept
+    inline void setstate(const state_type& seed) noexcept
     {
         MyBaseClass::_state.seed = seed;
         MyBaseClass::_state.gauss_valid = false;
     }
 
     /** @brief Restores the internal state of this PRNG from seed and gauss_next. */
-    inline  void setstate(const StateType& seed, const double gauss_next) noexcept
+    inline void setstate(const state_type& seed, const double gauss_next) noexcept
     {
         MyBaseClass::_state.seed = seed;
         MyBaseClass::_state.gauss_next = gauss_next;
@@ -193,7 +189,7 @@ public:
     *
     * @return a double value uniformly contained within range [0.0, 1.0).
     */
-    virtual const double random() noexcept override;
+    virtual const output_type next() noexcept override;
 
 
 protected:
@@ -202,51 +198,32 @@ protected:
     {
         MyBaseClass::_state.seed.index = _index % SIZE;
     }
+
+
+private:
+    static constexpr state_type _MODULO{ 0xffff'ffff'ffff'ffffull };
+
 };
 
 
 //===========================================================================
 //---   TEMPLATES IMPLEMENTATION   ------------------------------------------
 /** The internal PRNG algorithm. */
-template<const size_t SIZE, const size_t K>
-const double BaseLFib64<SIZE, K>::random() noexcept
+template<const size_t SIZE, const size_t K >
+const typename BaseLFib64<SIZE, K>::output_type BaseLFib64<SIZE, K>::next() noexcept
 {
     // evaluates indexes in suite for the i-5 and i-17 -th values
     const size_t index = MyBaseClass::_state.seed.index;
     const size_t k = (index < K) ? (index + SEED_SIZE) - K : index - K;
 
     // evaluates current value and modifies internal state
-    uint64_t value = MyBaseClass::_state.seed.list[k] + MyBaseClass::_state.seed.list[index];  // automatic 64-bits modulo
+    output_type value = MyBaseClass::_state.seed.list[k] + MyBaseClass::_state.seed.list[index];  // automatic 64-bits modulo
     MyBaseClass::_state.seed.list[index] = value;
 
     // next index
     MyBaseClass::_state.seed.index = (index + 1) % SEED_SIZE;
 
-    // finally, returns pseudo random value in range [0.0, 1.0)
-    return double((long double)value / (long double)18'446'744'073'709'551'616.0);
+    // finally, returns output value
+    return value;
 }
 
-
-/** Sets the internal state of this PRNG from current time (empty signature). */
-template<const size_t SIZE, const size_t K>
-void BaseLFib64<SIZE, K>::setstate() noexcept
-{
-    const uint64_t ticks = std::chrono::duration_cast<std::chrono::nanoseconds>(
-        std::chrono::high_resolution_clock::now().time_since_epoch()).count();
-
-    setstate(((ticks & 0x0000'0000'7fff'ffff) << 32) +
-        ((ticks & 0xff00'0000'0000'0000) >> 56) +
-        ((ticks & 0x00ff'0000'0000'0000) >> 40) +
-        ((ticks & 0x0000'ff00'0000'0000) >> 24) +
-        ((ticks & 0x0000'00ff'0000'0000) >> 8));
-}
-
-
-/** Sets the internal state of this PRNG with an integer seed. */
-template<const size_t SIZE, const size_t K>
-void BaseLFib64<SIZE, K>::setstate(const uint64_t seed) noexcept
-{
-    FastRand63 myRand(seed);
-    for (auto it = MyBaseClass::_state.seed.list.begin(); it != MyBaseClass::_state.seed.list.end(); )
-        *it++ = myRand(0x7fff'ffff'ffff'ffffULL) * 2 + myRand(2);
-}
