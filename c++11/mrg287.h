144784d9bc18e6818fa57a256a8716b7858e9366
Philippe Schmouker <ph.schmouker@yahoo.fr>
2025-03-16 19:42:43 +0100

    #95-refactor MRG287
    c++11 completed. Compiles. Still to be validated.

--- c++11/mrg287.h
+++ c++11/mrg287.h
@@ -75,24 +75,24 @@ SOFTWARE.
 *   and offers a period of about 2^287 - i.e. 2.49e+86 - with low computation time due
 *   to the use of a 2^32 modulo.
 *   
-*   See MRGRand1457 for a  longer  period  MR-Generator  (2^1457,  i.e. 4.0e+438)  and 
-*   longer  computation  time  (2^31-1  modulus  calculations)  but  less memory space 
-*   consumption (47 integers).
-*   See MRGRand49507 for a far longer period  (2^49_507,  i.e. 1.2e+14_903)  with  low 
-*   computation  time  too  (31-bits  modulus)  but  use  of  more memory space (1_597 
+*
+*   See Mrg1457 for a longer period MR-Generator  (2^1457,  i.e. 4.0e+438)  and longer
+*   computation  time  (2^31-1 modulus calculations) but less memory space consumption
+*   (i.e. 47 integers).
+*   See Mrg49507 for  a  far  longer  period  (2^49_507,  i.e. 1.2e+14_903)  with  low
+*   computation  time  too  (31-bits  modulus)  but  use  of  more memory space (1_597
 *   integers).
-*     
+*
 *   Furthermore this class is callable:
 * @code
-*     MRGRand287 rand();
+*     Mrg287 rand();
 *     std::cout << rand() << std::endl;    // prints a uniform pseudo-random value within [0.0, 1.0)
 *     std::cout << rand(b) << std::endl;   // prints a uniform pseudo-random value within [0.0, b)
-*     std::cout << rand(a,b) << std::endl; // prints a uniform pseudo-random value within [a  , b)
 * @endcode
-* 
+*
 *   Notice that for simulating the roll of a dice you should program:
 * @code
-*     MRGRand287 diceRoll();
+*     Mrg287 diceRoll();
 *     std::cout << int(diceRoll(1, 7)) << std::endl;    // prints a uniform roll within range {1, ..., 6}
 *     std::cout << diceRoll.randint(1, 6) << std::endl; // prints also a uniform roll within range {1, ..., 6}
 * @endcode
@@ -104,9 +104,9 @@ SOFTWARE.
 * +---------------------------------------------------------------------------------------------------------------------------------------------------+
 * | PyRabndLib class | TU01 generator name | Memory Usage    | Period  | time-32bits | time-64 bits | SmallCrush fails | Crush fails | BigCrush fails |
 * | ---------------- | ------------------- | --------------- | ------- | ----------- | ------------ | ---------------- | ----------- | -------------- |
-* | MRGRand287       | Marsa-LFIB4         |   256 x 4-bytes | 2^287   |    3.40     |     0.8      |          0       |       0     |       0        |
-* | MRGRand1457      | DX-47-3             |    47 x 4-bytes | 2^1457  |    n.a.     |     1.4      |          0       |       0     |       0        |
-* | MRGRand49507     | DX-1597-2-7         | 1,597 x 4-bytes | 2^49507 |    n.a.     |     1.4      |          0       |       0     |       0        |
+* | MRG287           | Marsa-LFIB4         |   256 x 4-bytes | 2^287   |    3.40     |     0.8      |          0       |       0     |       0        |
+* | MRG1457          | DX-47-3             |    47 x 4-bytes | 2^1457  |    n.a.     |     1.4      |          0       |       0     |       0        |
+* | MRG49507         | DX-1597-2-7         | 1,597 x 4-bytes | 2^49507 |    n.a.     |     1.4      |          0       |       0     |       0        |
 * +---------------------------------------------------------------------------------------------------------------------------------------------------+
 *
 *   * _small crush_ is a small set of simple tests that quickly tests some  of
@@ -118,7 +118,7 @@ SOFTWARE.
 *   * _big crush_ is the ultimate set of difficult tests  that  any  GOOD  PRG
 *   should definitively pass.
 */
-class MRGRand287 : public BaseMRG32<256>
+class Mrg287 : public BaseMRG32<256>
 {
 public:
     //---   Wrappers   ------------------------------------------------------
@@ -127,47 +127,47 @@ public:
 
     //---   Constructors / Destructor   -------------------------------------
     /** @brief Empty constructor. */
-    inline MRGRand287() noexcept
+    inline Mrg287() noexcept
         : MyBaseClass()
     {
         MyBaseClass::setstate();
     }
 
     /** @brief Valued construtor (integer). */
-    inline MRGRand287(const uint32_t seed) noexcept
+    inline Mrg287(const uint32_t seed) noexcept
         : MyBaseClass()
     {
         MyBaseClass::setstate(seed);
     }
 
     /** @brief Valued construtor (double). */
-    inline MRGRand287(const double seed) noexcept
+    inline Mrg287(const double seed) noexcept
         : MyBaseClass()
     {
         MyBaseClass::setstate(seed);
     }
 
     /** @brief Valued constructor (full state). */
-    inline MRGRand287(const StateType& seed) noexcept
+    inline Mrg287(const state_type& seed) noexcept
         : MyBaseClass()
     {
         setstate(seed);
     }
 
-    /** @brief Default Copy constructor. */
-    MRGRand287(const MRGRand287&) noexcept = default;
-
-    /** @brief Default Move constructor. */
-    MRGRand287(MRGRand287&&) noexcept = default;
-
-    /** @brief Default Destructor. */
-    virtual ~MRGRand287() noexcept = default;
+    Mrg287(const Mrg287&) noexcept = default;   //!< default copy constructor.
+    Mrg287(Mrg287&&) noexcept = default;        //!< default move constructor.
+    virtual ~Mrg287() noexcept = default;       //!< default destructor.
 
 
     //---   Internal PRNG   -------------------------------------------------
     /** @brief The internal PRNG algorithm.
     *
-    * @return a double value uniformly contained within range [0.0, 1.0).
+    * @return an integer value coded on OUTPUT_BITS bits.
     */
-    virtual const double random() noexcept override;
+    virtual const output_type next() noexcept override;
+
+
+private:
+    static constexpr std::uint32_t _INDEX_MODULO{ 0xff };  // related to SEED_SIZE = 256
+
 };
