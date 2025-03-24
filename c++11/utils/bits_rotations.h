9d8c745e8580f78275b718a113295dfc24d1a54d
Philippe Schmouker <ph.schmouker@yahoo.fr>
2025-03-24 07:03:39 +0100

    #171-implement left rotation function in namespace utils
    Fully completed. Validated.

---
+++ c++11/utils/bits_rotations.h
@@ -0,0 +1,52 @@
+#pragma once
+/*
+MIT License
+
+Copyright (c) 2025 Philippe Schmouker, ph.schmouker (at) gmail.com
+
+This file is part of library CppRandLib.
+
+Permission is hereby granted,  free of charge,  to any person obtaining a copy
+of this software and associated documentation files (the "Software"),  to deal
+in the Software without restriction,  including without limitation the  rights
+to use,  copy,  modify,  merge,  publish,  distribute, sublicense, and/or sell
+copies of the Software,  and  to  permit  persons  to  whom  the  Software  is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS",  WITHOUT WARRANTY OF ANY  KIND,  EXPRESS  OR
+IMPLIED,  INCLUDING  BUT  NOT  LIMITED  TO  THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT  SHALL  THE
+AUTHORS  OR  COPYRIGHT  HOLDERS  BE  LIABLE  FOR  ANY CLAIM,  DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,  ARISING FROM,
+OUT  OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
+//===========================================================================
+#include <cassert>
+#include <type_traits>
+
+
+//===========================================================================
+namespace utils
+{
+    //=======================================================================
+    /** @brief Bits left rotation on unsigned integers. */
+    template<typename IntT>
+    inline const IntT rot_left(const IntT value, const int rot_count, const int BITS_COUNT = 8 * sizeof IntT)
+    {
+        static_assert(std::is_unsigned<IntT>::value, "bits rotation are only applied on unsigned integer values.");
+        assert(rot_count >= 1);
+        assert(rot_count <= bits_count);
+
+        const IntT lo_mask{ IntT((1 << (BITS_COUNT - rot_count)) - 1) };
+        const IntT hi_mask{ ((1 << BITS_COUNT) - 1) ^ lo_mask };
+
+        return ((value & lo_mask) << rot_count) | ((value & hi_mask) >> (BITS_COUNT - rot_count));
+    }
+
+}
