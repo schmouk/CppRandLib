0bb150488550085ff3bab4d79cf095d10ac559f0
Philippe Schmouker <ph.schmouker@yahoo.fr>
2025-03-24 05:43:16 +0100

    #166-c++20 code review
    minor modifications.

--- c++20/baselfib64.h
+++ c++20/baselfib64.h
@@ -197,7 +197,7 @@ const typename BaseLFib64<SIZE, K>::output_type BaseLFib64<SIZE, K>::next() noex
     // next index
     MyBaseClass::_internal_state.state.index = (index + 1) % SEED_SIZE;
 
-    // finally, returns output value
+    // finally, returns pseudo random value as a 64-bits integer
     return value;
 }
 
