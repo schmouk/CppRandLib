5d115c57fd464ec8ef585a90d2fb52c1a228d259
Philippe Schmouker <ph.schmouker@yahoo.fr>
2025-03-26 18:38:32 +0100

    #179-implement class CounterKeyState
    refactored implementation of method `init_key()`.

--- c++11/counterkeystate.h
+++ c++11/counterkeystate.h
@@ -29,6 +29,7 @@ SOFTWARE.
 //===========================================================================
 #include <algorithm>
 #include <type_traits>
+#include <vector>
 
 #include "utils/seed_generation.h"
 #include "utils/splitmix.h"
@@ -36,47 +37,56 @@ SOFTWARE.
 
 //===========================================================================
 /** @brief The internal state of counter-based Pseudo Random Numbers Generators. */
-template<typename CounterType, typename KeyType = CounterType>
+template<typename CounterType>
 struct CounterKeyState
 {
     static_assert(std::is_integral<CounterType>::value);
-    static_assert(std::is_integral<KeyType>::value);
 
     CounterType counter{ 0 };
-    KeyType key{ 0 };
+    CounterType key{ 0 };
 
-    inline CounterKeyState(const CounterType c, const KeyType k) noexcept
+    inline CounterKeyState(const CounterType c, const CounterType k) noexcept
         : counter(c)
         , key(k)
     {}
 
 
     inline void init_key() noexcept
-    {
-        init_key(utils::set_random_seed32());
-    }
+    {}
 
     /** @brief Initalizes the attribute key according to the original recommendations in document [9] - see file REAME.md. */
-    void init_key(const std::uint32_t seed) noexcept;
+    inline void init_key(const CounterType seed) noexcept;
+
 };
 
 
 //===========================================================================
 //---   TEMPLATES IMPLEMENTATION   ------------------------------------------
 //---------------------------------------------------------------------------
-template<typename CounterType, typename KeyType>
-void CounterKeyState<CounterType, KeyType>::init_key(
+template<typename CounterType>
+void CounterKeyState<CounterType>::init_key(
+    const CounterType seed
+) noexcept
+{
+    // default implementation - see specializations
+    key = 1;
+}
+
+
+//---------------------------------------------------------------------------
+template<>
+void CounterKeyState<std::uint32_t>::init_key(
     const std::uint32_t seed
 ) noexcept
 {
-    constexpr float NORMALIZE{ 1.0f / float(1ull << (8 * sizeof(KeyType)) };
+    constexpr double NORMALIZE{ 1.0 / double(0x1'0000'0000ull) };
 
-    KeyType hex_digits[]{ 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf };
+    std::vector<std::uint32_t> hex_digits{ 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf };
     utils::SplitMix32 init_rand(seed);
 
     key = 0;
 
-    // let's initialize the 8 high hexa digits of the key - all different
+    // let's initialize the 8 hexa digits of the key - all different
     for (std::uint64_t n = 15; n >= 8; --n) {
         const std::uint64_t i{ std::uint64_t(double(n) * double(init_rand()) * NORMALIZE) };
 
@@ -84,9 +94,34 @@ void CounterKeyState<CounterType, KeyType>::init_key(
         std::swap(hex_digits[i], hex_digits[n]);
     }
 
+    key |= 1;  // notice: key must be odd
+
+}
+
+//---------------------------------------------------------------------------
+template<>
+void CounterKeyState<std::uint64_t>::init_key(
+    const std::uint64_t seed
+) noexcept
+{
+    constexpr double NORMALIZE{ 0.5 / double(0x8000'0000'0000'0000ull) };
+
+    std::vector<std::uint32_t> hex_digits{ 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf };
+    utils::SplitMix64 init_rand(seed);
+
+    key = 0;
+
+    // let's initialize the 8 high hexa digits of the key - all different
+    for (std::uint32_t n = 15; n >= 8; --n) {
+        const std::uint32_t i{ std::uint32_t(double(n) * double(init_rand()) * NORMALIZE) };
+
+        key = (key << 4) + hex_digits[i];
+        std::swap(hex_digits[i], hex_digits[n]);
+    }
+
     // then let's initialize the 8 low hexa digits of the key - all different
-    for (std::uint64_t n = 15; n >= 8; --n) {
-        const std::uint64_t i{ std::uint64_t(double(n) * double(init_rand()) * NORMALIZE) };
+    for (std::uint32_t n = 15; n >= 8; --n) {
+        const std::uint32_t i{ std::uint32_t(double(n) * double(init_rand()) * NORMALIZE) };
 
         key = (key << 4) + hex_digits[i];
         std::swap(hex_digits[i], hex_digits[n]);
