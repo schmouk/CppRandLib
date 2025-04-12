1b7e842fe4523ddc0312e9276488567a61297cca
Philippe Schmouker <ph.schmouker@yahoo.fr>
2025-04-12 21:27:09 +0200

    #206-unit test module utils/seed_generation.h
    Fully validated.

---
+++ gTests-c++11/test_utils_seed_generation.cpp
@@ -0,0 +1,112 @@
+/*
+MIT License
+
+Copyright (c) 2025 Philippe Schmouker, ph.schmouker (at) gmail.com
+
+This file is part of library CppRandLib.
+
+Permission is hereby granted,  free of charge,  to any person obtaining a copy
+of this software and associated documentation files (the "Software"),  to deal
+in the Software without restriction,  including without limitation the  rights
+to use,  copy,  modify,  merge,  publish,  distribute, sublicense, and/or sell
+copies of the Software,  and  to  permit  persons  to  whom  the  Software  is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS",  WITHOUT WARRANTY OF ANY  KIND,  EXPRESS  OR
+IMPLIED,  INCLUDING  BUT  NOT  LIMITED  TO  THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT  SHALL  THE
+AUTHORS  OR  COPYRIGHT  HOLDERS  BE  LIABLE  FOR  ANY CLAIM,  DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,  ARISING FROM,
+OUT  OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
+//===========================================================================
+#include <cstdint>
+
+#include "gtest/gtest.h"
+
+#include "utils/seed_generation.h"
+
+
+//===========================================================================
+namespace tests_utils
+{
+    //-----------------------------------------------------------------------
+    TEST(TestSuiteUtils, TestsUtilsSeedGeneration)
+    {
+        // checks correctness of utils::set_random_seed64
+        {
+            std::uint64_t val_prec{ utils::set_random_seed64() - 1 };
+            std::uint64_t val{};
+
+            for (int i = 0; i < 100'000; ++i) {
+                val = utils::set_random_seed64() + i;
+                EXPECT_NE(val_prec, val);
+                val_prec = val;
+            }
+        }
+
+        // checks correctness of utils::set_random_seed63
+        {
+            std::uint64_t val_prec{ utils::set_random_seed63() };
+            EXPECT_LE(val_prec, 0x7fff'ffff'ffff'ffffull);
+
+            val_prec = (val_prec - 1) & 0x7fff'ffff'ffff'ffffull;
+            std::uint64_t val{};
+
+            for (int i = 0; i < 100'000; ++i) {
+                val = utils::set_random_seed63();
+                EXPECT_LE(val, 0x7fff'ffff'ffff'ffffull);
+
+                val = (val + i) & 0x7fff'ffff'ffff'ffffull;
+                EXPECT_NE(val_prec, val);
+
+                val_prec = val;
+            }
+        }
+
+        // checks correctness of utils::set_random_seed32
+        {
+            std::uint64_t val_prec{ utils::set_random_seed32() };
+            EXPECT_LE(val_prec, 0xffff'ffffull);
+
+            val_prec = (val_prec - 1) & 0xffff'ffffull;
+            std::uint64_t val{};
+
+            for (int i = 0; i < 100'000; ++i) {
+                val = utils::set_random_seed32();
+                EXPECT_LE(val, 0xffff'ffffull);
+
+                val = (val + i) & 0xffff'ffffull;
+                EXPECT_NE(val_prec, val);
+
+                val_prec = val;
+            }
+        }
+
+        // checks correctness of utils::set_random_seed31
+        {
+            std::uint64_t val_prec{ utils::set_random_seed31() };
+            EXPECT_LE(val_prec, 0x7fff'ffffull);
+
+            val_prec = (val_prec - 1) & 0x7fff'ffffull;
+            std::uint64_t val{};
+
+            for (int i = 0; i < 100'000; ++i) {
+                val = utils::set_random_seed31();
+                EXPECT_LE(val, 0x7fff'ffffull);
+
+                val = (val + i) & 0x7fff'ffffull;
+                EXPECT_NE(val_prec, val);
+
+                val_prec = val;
+            }
+        }
+
+    }
+}
